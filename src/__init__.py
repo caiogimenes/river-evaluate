9816a4874be7a7db03ec45dab82accc5dd2a2a45
caiogimenes <ciaonemegi@gmail.com>
2025-12-24 16:09:47 -0300

    Updates src to create interface for newer tools

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,8 @@
-from .utils import evaluate, rank_logs, friedman_statistics, iman_davenport, run_prequential_eval
+from .utils import evaluate, rank_logs, run_prequential_eval
 __all__ = [
     "data",
     "plot",
     "evaluate",
-    "friedman_statistics",
-    "iman_davenport",
     "rank_logs",
     "run_prequential_eval"
 ]
