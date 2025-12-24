89d1010623f5564620666562840c56900e388e85
caiogimenes <ciaonemegi@gmail.com>
2025-12-24 16:09:09 -0300

    Removes statistical tools from general utils

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from pandas import DataFrame
 from src.data import RunnerLog
 from river.metrics import RMSE
 
+
 def rank_logs(logs: List[RunnerLog], att: str, models, datasets):
     models = list(models)
     datasets = list(datasets)
@@ -20,35 +21,6 @@ def rank_logs(logs: List[RunnerLog], att: str, models, datasets):
             friedman_matrix[dataset_idx, model_idx] = log.learn_time[-1]
 
     return DataFrame(friedman_matrix, columns=models)
-
-
-def friedman_statistics(avg_rank, N: int):
-    """
-    Calculates the Friedman statistic.
-    :param avg_rank: list or array of average ranks for each algorithm
-    :param N: number of datasets (rows)
-    """
-    avg_rank = np.array(avg_rank)
-    k = len(avg_rank)
-
-    # Calculate Friedman statistic
-    chi_sq = (12 * N / (k * (k + 1))) * (np.sum(avg_rank ** 2) - (k * (k + 1) ** 2) / 4)
-    return chi_sq
-
-
-def iman_davenport(chi_sq, N: int, k: int):
-    """
-    Calculates the Iman-Davenport statistic.
-    :param chi_sq: The result from the Friedman statistic
-    :param N: number of datasets
-    :param k: number of algorithms (columns)
-    """
-    # Prevent division by zero if predictions are identical (denominator becomes 0)
-    denominator = (N * (k - 1) - chi_sq)
-    if denominator == 0:
-        return np.inf
-
-    return (N - 1) * chi_sq / denominator
 
 def evaluate(dataset, model, metric, print_every=10):
     """
